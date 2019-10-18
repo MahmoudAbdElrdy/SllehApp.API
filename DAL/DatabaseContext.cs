ccf37043d601d85fbfa0450dc664246d3f58493d
Mahmoud <mahmoudabd23451@gmail.com>
2019-10-19 00:33:33 +0200

    CreateDatabase

---
+++ DAL/DatabaseContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace DAL
+{
+    public class DatabaseContext: IdentityDbContext
+    {
+        public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options)
+        {
+
+        }
+    }
+}
